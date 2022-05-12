1815cdaa8e0faab77536acfe8d21281ec65e7f64
Juan Lorthiois <lor18771@uvg.edu.gt>
2022-05-12 06:58:29 -0600

    Implmenetacion de la primera version del sistema de recomendaciones con GUI y conexion a BD de Neo4j en la nube mediante auraDB

---
+++ src/main/java/DB_Control/Libros.java
@@ -0,0 +1,145 @@
+package DB_Control;
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Result;
+import org.neo4j.driver.Session;
+import static org.neo4j.driver.Values.parameters;
+import org.neo4j.driver.Record;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+//ejemplo consultado para estabecer conexion a DB: https://github.com/neo4j/neo4j-documentation/blob/4.4/cypher/cypher-docs/src/main/java/org/neo4j/cypher/example/JavaQuery.java
+
+
+public class Libros implements AutoCloseable {
+	
+	
+	private final Driver driver;
+	
+	private static HashMap<Integer, ArrayList<ArrayList<String>>> recomendaciones = new HashMap<>();
+	
+	
+	
+
+    public Libros( String uri, String user, String password )
+    {
+        driver = GraphDatabase.driver( uri, AuthTokens.basic( user, password ) );
+    }
+	
+	
+    public void close() throws Exception
+    {
+        driver.close();
+    }
+
+    public void query( String query )
+    {
+        try ( Session session = driver.session() )
+        {
+             session.writeTransaction( tx ->
+                                                        {
+                                                            Result result = tx.run( "match (a:Book {title: $title})\r\n"
+                                                            		+ "match (b:Book) where b.description <> a.description\r\n"
+                                                            		+ "\r\n"
+                                                            		+ "with a, b, apoc.text.clean(a.description) as norm1, apoc.text.clean(b.description) as norm2\r\n"
+                                                            		+ "with toInteger(apoc.text.jaroWinklerDistance(norm1, norm2) * 100) as similarity, a, b\r\n"
+                                                            		+ "with a, b,similarity where similarity >= 75 and a.categories = b.categories and a.average_rating <= b.average_rating\r\n"
+                                                            		+ "return a.title as TITULO_INGRESADO, b.title as RECOMENDACION, b.categories as GENERO, b.average_rating as RATING, b.authors as AUTORES, b.num_pages as Q_PAGINAS, similarity\r\n"
+                                                            		, parameters( "title", query ) );
+                                                            
+                                                            
+                                                            
+                                                            while (result.hasNext()) {
+                                                            	
+                                                            	Record record = result.next();
+                                                            	String titulo_recomendado = record.get("RECOMENDACION").asString();
+                                                            	String autor = record.get("AUTORES").asString();
+                                                            	int similarity = record.get("similarity").asInt();
+                                                            	
+                                                            	
+                                                            	ArrayList<String> titulo_autor = new ArrayList<String>();
+                                                            	titulo_autor.add(titulo_recomendado);
+                                                            	titulo_autor.add(autor);
+                                                            	
+                                                            	ArrayList<ArrayList<String>> new_val = new ArrayList<>();
+                                                            	new_val.add(titulo_autor);
+                                                            	
+                                                            	
+                                                            	if (recomendaciones.containsKey(similarity)) {
+                                                            		
+                                                            		ArrayList<ArrayList<String>> value = new ArrayList<ArrayList<String>>();
+                                                            		value = recomendaciones.get(similarity);
+                                                            		value.add(titulo_autor);
+                                                            		
+                                                            		recomendaciones.replace(similarity, value);
+                                                            	}else {
+                                                            		
+                                                            		recomendaciones.put(similarity, new_val );
+                                                            		
+                                                            	}
+                                                            	
+                                                            	
+                                                            }
+                                                                                                                        
+                                                            return result;
+                                                            
+                                                        } );
+            
+            
+            
+            
+        }
+    }
+
+    
+    public static ArrayList<ArrayList<String>> getRecs(){
+    	
+    	System.out.println("TEST");
+    	
+    	ArrayList<ArrayList<String>> recs_final = new  ArrayList<>();
+    	
+    	
+    	for (int i = 100; i >= 75; i--) {
+    		
+    		if (recomendaciones.containsKey(i)) {
+    			
+    			for (int j = 0; j < recomendaciones.get(i).size(); j ++){
+    				
+    				ArrayList<String> recom_sim = new ArrayList<String>();
+    				Integer similarity = i;
+    				
+    				recom_sim.add(recomendaciones.get(i).get(j).get(0));
+    				recom_sim.add(recomendaciones.get(i).get(j).get(1));
+    				recom_sim.add(similarity.toString());
+    				
+    				recs_final.add(recom_sim);
+    					
+    			}
+    			
+    		}
+    		
+    		if (recs_final.size() > 5) {
+    			
+    			i = 0;
+    		}
+    	}
+    	
+    	recomendaciones.clear();
+    	System.out.println(recs_final);
+    	return recs_final;
+    	
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ src/main/java/GUI/Generador.java
@@ -0,0 +1,146 @@
+package GUI;
+import DB_Control.Libros;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+//fuente consultada para implementar GUI: https://www.youtube.com/watch?v=iE8tZ0hn2Ws&t=936s
+
+ 
+
+
+
+public class Generador implements ActionListener {
+
+	private static JLabel label;
+	private static JLabel rec;
+	private static JLabel rec1;
+	private static JLabel rec2;
+	private static JLabel rec3;
+	private static JLabel rec4;
+	private static JLabel rec5;
+	private static JTextField titulo;
+	private static JButton boton;
+	
+	private static JPanel panel = new JPanel();
+	private static JFrame frame = new JFrame();
+	
+	private static ArrayList<ArrayList<String>> recomendaciones = new ArrayList<ArrayList<String>>();
+	
+	public static void prompt(){
+		
+		
+		
+		frame.setSize(1000,400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		
+		panel.setLayout(null);
+		
+		label = new JLabel("LIBRO QUE TE GUSTA");
+		label.setBounds(55,30,150,25);
+		panel.add(label);
+		
+		titulo = new JTextField(20);
+		titulo.setBounds(200,30,165,25);
+		panel.add(titulo);
+		
+		
+		boton = new JButton("RECOMENDAR MAS!");
+		boton.setBounds(405, 30, 180, 25);
+		boton.addActionListener(new Generador());
+		panel.add(boton);
+		
+		rec = new JLabel("");
+		rec.setBounds(120,50,1000,50);
+		panel.add(rec);
+		
+		rec1 = new JLabel("");
+		rec1.setBounds(120,80,1000,50);
+		panel.add(rec1);
+		
+		rec2 = new JLabel("");
+		rec2.setBounds(120,110,1000,50);
+		panel.add(rec2);
+		
+		rec3 = new JLabel("");
+		rec3.setBounds(120,140,1000,50);
+		panel.add(rec3);
+		
+		rec4 = new JLabel("");
+		rec4.setBounds(120,170,1000,50);
+		panel.add(rec4);
+		
+		rec5 = new JLabel("");
+		rec5.setBounds(120,200,1000,50);
+		panel.add(rec5);
+		
+		frame.setVisible(true);
+		
+	}
+	
+	
+	public void actionPerformed(ActionEvent e) {
+		
+		rec1.setText("");
+		rec2.setText("");
+		rec3.setText("");
+		rec4.setText("");
+		rec5.setText("");
+		
+		
+		try {
+		recomendaciones();
+		} catch (Exception E) {
+			
+			E.printStackTrace();
+			
+		}
+	}
+	
+	
+	public static void recomendaciones() throws Exception {
+		
+		try ( Libros libros = new Libros( "neo4j+s://e8cabdf1.databases.neo4j.io", "neo4j", "iZwumGTsvgEW3Cp4XQdhvpVs24KzeW8FSJ4M0lnLDLw" ) )
+        {
+			
+			libros.query(titulo.getText().toLowerCase());
+			recomendaciones = Libros.getRecs();
+        }
+		
+		rec.setText("Generando recomendaciones para: " + titulo.getText());
+		titulo.setText(null);
+		rec1.setText("Libro 1: " + recomendaciones.get(0).get(0).toUpperCase() + " de " + recomendaciones.get(0).get(1) + "   Similarity: " + recomendaciones.get(0).get(2));
+		rec2.setText("Libro 2: " + recomendaciones.get(1).get(0).toUpperCase() + " de " + recomendaciones.get(1).get(1) + "   Similarity: " + recomendaciones.get(1).get(2));
+		rec3.setText("Libro 3: " + recomendaciones.get(2).get(0).toUpperCase() + " de " + recomendaciones.get(2).get(1) + "   Similarity: " + recomendaciones.get(2).get(2));
+		rec4.setText("Libro 4: " + recomendaciones.get(3).get(0).toUpperCase() + " de " + recomendaciones.get(3).get(1) + "   Similarity: " + recomendaciones.get(3).get(2));
+		rec5.setText("Libro 5: " + recomendaciones.get(4).get(0).toUpperCase() + " de " + recomendaciones.get(4).get(1) + "   Similarity: " + recomendaciones.get(4).get(2));
+		
+		recomendaciones.clear();
+	}
+	
+	
+	
+	public static void main(String[] args) throws Exception {
+		
+		prompt();
+		
+		
+	    }
+		
+	
+
+	
+	
+	
+	
+	
+	
+}
